10c6a15ef7eed9798ea50374609b3788d0a1ee40
litepresence <rpfpresence@gmail.com>
2018-03-19 14:45:40 -0400

    Update book.py

--- EV/book.py
+++ EV/book.py
@@ -1,9 +1,12 @@
 # creates output file book.txt of latest dex orderbook for given market
 
-# checks 5 nodes, seeks 3 the same
-# if no 3 then gives matching 2
-# if no matching 2 then gives columnwise median
-# if anything goes wrong w/ median, renders statistical reconstruction 
+# 90% checks 5 nodes, seeks 3 the same
+# 4% if no 3 then gives matching 2
+# 3% if no matching 2 then gives columnwise median
+# 1! if anything goes wrong w/ median, renders statistical reconstruction 
+# averages 15 seconds per loop
+
+
 
 # alpha testing
 
