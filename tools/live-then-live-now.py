48ea5ee56030c4240cdaaf7ad79734c3a12e8212
litepresence <rpfpresence@gmail.com>
2018-12-24 15:13:53 -0500

    Update and rename EV/live-then-live-now.py to tools/live-then-live-now.py

---
+++ tools/live-then-live-now.py
@@ -0,0 +1,119 @@
+'litepresence2018'
+
+#compare three lists
+print("\033c")
+import zlib
+logo = b'x\x9c\xad\xd4M\n\xc4 \x0c\x05\xe0}O1P\x12B\x10\xbc\x82\xf7?\xd5\xf8\xaf\x83F\xe3\xe0[t\xf9\xf5%\xda>\x9f\x1c\xf7\x7f\x9e\xb9\x01\x17\x0cc\xec\x05\xe3@Y\x18\xc6(\'Z\x1a\xca.\x1bC\xa5l\r\x85\xa20\xb6\x8a\xca\xd8,W0\xec\x05\xc3\xdf\xd4_\xe3\r\x11(q\x16\xec\x95l\x04\x06\x0f\x0c\xc3\xddD\x9dq\xd2#\xa4NT\x0c/\x10\xd1{b\xd4\x89\x92\x91\x84\x11\xd9\x9d-\x87.\xe4\x1cB\x15|\xe0\xc8\x88\x13\xa5\xbc\xd4\xa21\x8e"\x18\xdc\xd2\x0e\xd3\xb6\xa0\xc6h\xa3\xd4\xde\xd0\x19\x9a\x1e\xd8\xddr\x0e\xcf\xf8n\xe0Y\rq\x1fP:p\x92\xf2\xdbaB,v\xda\x84j\xc4.\x03\xb1>\x97\xee{\x99oSa\x00\x0f\xc6\x84\xd8\xdf\x0f\xb4e\xa7$\xfdE\xae\xde\xb1/\x1d\xfc\x96\x8a'
+print(zlib.decompress(logo).decode())
+
+# LIVE THEN
+# MAX LIVE SINCE 181127
+a = ['wss://australia.bitshares.apasia.tech', 'wss://ws.winex.pro','wss://altcap.io', 'wss://api-ru.bts.blckchnd.com', 'wss://api.bitshares.bhuz.info', 'wss://api.bitsharesdex.com', 'wss://api.bts.ai', 'wss://api.bts.blckchnd.com', 'wss://api.bts.mobi', 'wss://api.bts.network', 'wss://api.btsgo.net', 'wss://api.btsxchng.com', 'wss://api.dex.trading', 'wss://api.fr.bitsharesdex.com', 'wss://atlanta.bitshares.apasia.tech', 'wss://australia.bitshares.apasia.tech', 'wss://b.mrx.im', 'wss://bit.btsabc.org', 'wss://bitshares.cyberit.io', 'wss://bitshares.dacplay.org', 'wss://bitshares.dacplay.org:8089', 'wss://bitshares.openledger.info', 'wss://blockzms.xyz', 'wss://bts-api.lafona.net', 'wss://bts-seoul.clockwork.gr', 'wss://bts.liuye.tech:4443', 'wss://bts.open.icowallet.net', 'wss://bts.proxyhosts.info', 'wss://btsfullnode.bangzi.info', 'wss://btsws.roelandp.nl', 'wss://chicago.bitshares.apasia.tech', 'wss://citadel.li/node', 'wss://crazybit.online', 'wss://dallas.bitshares.apasia.tech', 'wss://dex.rnglab.org', 'wss://dexnode.net', 'wss://england.bitshares.apasia.tech', 'wss://eu-central-1.bts.crypto-bridge.org', 'wss://eu.nodes.bitshares.ws', 'wss://eu.openledger.info', 'wss://france.bitshares.apasia.tech', 'wss://frankfurt8.daostreet.com', 'wss://kc-us-dex.xeldal.com', 'wss://kimziv.com', 'wss://la.dexnode.net', 'wss://miami.bitshares.apasia.tech', 'wss://na.openledger.info', 'wss://ncali5.daostreet.com', 'wss://netherlands.bitshares.apasia.tech', 'wss://new-york.bitshares.apasia.tech', 'wss://node.bitshares.eu', 'wss://node.market.rudex.org', 'wss://nohistory.proxyhosts.info', 'wss://openledger.hk', 'wss://paris7.daostreet.com', 'wss://relinked.com', 'wss://scali10.daostreet.com', 'wss://seattle.bitshares.apasia.tech', 'wss://sg.nodes.bitshares.ws', 'wss://singapore.bitshares.apasia.tech', 'wss://status200.bitshares.apasia.tech', 'wss://us-east-1.bts.crypto-bridge.org', 'wss://us-la.bitshares.apasia.tech', 'wss://us-ny.bitshares.apasia.tech', 'wss://us.nodes.bitshares.ws', 'wss://valley.bitshares.apasia.tech', 'wss://ws.gdex.io', 'wss://ws.gdex.top', 'wss://ws.hellobts.com']
+
+# UI MASTER LIST
+b = ['wss://ap-northeast-1.bts.crypto-bridge.org', 'wss://ap-southeast-1.bts.crypto-bridge.org', 'wss://ap-southeast-2.bts.crypto-bridge.org', 'wss://api-ru.bts.blckchnd.com', 'wss://api.bitshares.bhuz.info', 'wss://api.bitsharesdex.com', 'wss://api.bts.ai', 'wss://api.bts.blckchnd.com', 'wss://api.bts.mobi', 'wss://api.bts.network', 'wss://api.btsgo.net', 'wss://api.btsxchng.com', 'wss://api.dex.trading', 'wss://api.fr.bitsharesdex.com', 'wss://api.open-asset.tech', 'wss://atlanta.bitshares.apasia.tech', 'wss://australia.bitshares.apasia.tech', 'wss://bit.btsabc.org', 'wss://bitshares.bts123.cc:15138', 'wss://bitshares.crypto.fans', 'wss://bitshares.cyberit.io', 'wss://bitshares.nu', 'wss://bitshares.openledger.info', 'wss://blockzms.xyz', 'wss://bts-api.lafona.net', 'wss://bts-seoul.clockwork.gr', 'wss://bts.liuye.tech:4443', 'wss://bts.open.icowallet.net', 'wss://bts.proxyhosts.info', 'wss://btsfullnode.bangzi.info', 'wss://btsws.roelandp.nl', 'wss://canada6.daostreet.com', 'wss://chicago.bitshares.apasia.tech', 'wss://citadel.li/node', 'wss://crazybit.online', 'wss://dallas.bitshares.apasia.tech', 'wss://de.bts.dcn.cx', 'wss://dex.rnglab.org', 'wss://dexnode.net', 'wss://england.bitshares.apasia.tech', 'wss://eu-central-1.bts.crypto-bridge.org', 'wss://eu-west-1.bts.crypto-bridge.org', 'wss://eu-west-2.bts.crypto-bridge.org', 'wss://eu.nodes.bitshares.ws', 'wss://fi.bts.dcn.cx', 'wss://france.bitshares.apasia.tech', 'wss://freedom.bts123.cc:15138', 'wss://japan.bitshares.apasia.tech', 'wss://kc-us-dex.xeldal.com', 'wss://kimziv.com', 'wss://la.dexnode.net', 'wss://miami.bitshares.apasia.tech', 'wss://na.openledger.info', 'wss://netherlands.bitshares.apasia.tech', 'wss://new-york.bitshares.apasia.tech', 'wss://node.btscharts.com', 'wss://node.market.rudex.org', 'wss://node.testnet.bitshares.eu', 'wss://openledger.hk', 'wss://seattle.bitshares.apasia.tech', 'wss://sg.nodes.bitshares.ws', 'wss://status200.bitshares.apasia.tech', 'wss://testnet-eu.bitshares.apasia.tech', 'wss://testnet.bitshares.apasia.tech', 'wss://testnet.bts.dcn.cx', 'wss://testnet.dex.trading', 'wss://testnet.nodes.bitshares.ws', 'wss://us-east-1.bts.crypto-bridge.org', 'wss://us-la.bitshares.apasia.tech', 'wss://us-west-1.bts.crypto-bridge.org', 'wss://us.nodes.bitshares.ws', 'wss://valley.bitshares.apasia.tech', 'wss://ws.gdex.top', 'wss://ws.hellobts.com', 'wss://ws.winex.pro']
+
+# LIVE NOW
+c = ['wss://new-york.bitshares.apasia.tech/wss', 'wss://status200.bitshares.apasia.tech/ws', 'wss://dexnode.net/wss', 'wss://seattle.bitshares.apasia.tech/wss', 'wss://node.bitshares.eu/wss', 'wss://api.bts.mobi', 'wss://dex.rnglab.org/ws', 'wss://eu.nodes.bitshares.ws', 'wss://miami.bitshares.apasia.tech/ws', 'wss://kc-us-dex.xeldal.com/ws', 'wss://na.openledger.info/ws', 'wss://england.bitshares.apasia.tech/ws', 'wss://bitshares.openledger.info/wss', 'wss://frankfurt8.daostreet.com/ws', 'wss://citadel.li/node/ws', 'wss://dallas.bitshares.apasia.tech/wss', 'wss://altcap.io', 'wss://us-east-1.bts.crypto-bridge.org/ws', 'wss://api.bts.network/wss', 'wss://france.bitshares.apasia.tech/wss', 'wss://chicago.bitshares.apasia.tech/wss', 'wss://ws.winex.pro/wss', 'wss://ncali5.daostreet.com/wss', 'wss://paris7.daostreet.com/ws', 'wss://btsfullnode.bangzi.info/ws', 'wss://netherlands.bitshares.apasia.tech/wss', 'wss://us-la.bitshares.apasia.tech/wss', 'wss://api.bitsharesdex.com/ws', 'wss://atlanta.bitshares.apasia.tech/ws', 'wss://api.bts.blckchnd.com/wss', 'wss://eu-central-1.bts.crypto-bridge.org', 'wss://node.market.rudex.org', 'wss://api.bitshares.bhuz.info/ws', 'wss://api.fr.bitsharesdex.com', 'wss://us.nodes.bitshares.ws', 'wss://valley.bitshares.apasia.tech/ws', 'wss://us-ny.bitshares.apasia.tech/wss', 'wss://bts-api.lafona.net/ws', 'wss://btsws.roelandp.nl/ws', 'wss://bts-seoul.clockwork.gr/wss', 'wss://bts.open.icowallet.net/wss', 'wss://scali10.daostreet.com/wss', 'wss://bts.proxyhosts.info/wss', 'wss://ws.hellobts.com/ws', 'wss://blockzms.xyz/ws', 'wss://la.dexnode.net/wss', 'wss://ws.gdex.top/wss', 'wss://bitshares.cyberit.io/ws', 'wss://api.dex.trading/ws', 'wss://nohistory.proxyhosts.info/wss', 'wss://openledger.hk/ws', 'wss://singapore.bitshares.apasia.tech/ws', 'wss://ws.gdex.io/ws', 'wss://api.bts.ai/wss']
+
+
+
+def validate(parsed):
+    v = parsed
+    for i in range(len(v)):
+        if v[i].endswith('/'):
+            v[i] = v[i][:-1]
+    for i in range(len(v)):
+        if v[i].endswith('/ws'):
+            v[i] = v[i][:-3]
+    for i in range(len(v)):
+        if v[i].endswith('/wss'):
+            v[i] = v[i][:-4]
+    v = [i.replace(' ', '') for i in v]
+    return sorted(list(set(v)))
+
+a = validate(a)
+b = validate(b)
+c = validate(c)
+
+print('==========================================')
+print('A B C LISTS TO COMPARE')
+print('==========================================')
+print('')
+print('LIVE THEN: SEEN LIVE AT SOME POINT SINCE 181127')
+print('list a: ', len(a))
+print(a)
+print('')
+print('UI MASTER LIST')
+print('list b:', len(b))
+print(b)
+print('')
+print('LIVE NOW')
+print('list c:', len(c))
+print(c)
+print('')
+print('==========================================')
+print('THEN VS NOW')
+print('==========================================')
+print('')
+print('LIVE THEN AND LIVE NOW')
+print('[i for i in a if i in c]')
+x = ([i for i in a if i in c])
+print(len(x), x)
+print('')
+print('LIVE THEN NOT LIVE NOW')
+print('[i for i in a if i not in c]')
+x = ([i for i in a if i not in c])
+print(len(x), x)
+print('')
+print('LIVE NOW BUT NOT LIVE THEN')
+print('[i for i in c if i not in a]')
+x = ([i for i in c if i not in a])
+print(len(x), x)
+print('')
+print('==========================================')
+print('THEN VS UI MASTER')
+print('==========================================')
+print('')
+print('LIVE THEN AND IN UI MASTER')
+print('[i for i in a if i in b]')
+x = ([i for i in a if i in b])
+print(len(x), x)
+print('')
+print('LIVE THEN NOT IN UI MASTER')
+print('[i for i in a if i not in b]')
+x = ([i for i in a if i not in b])
+print(len(x), x)
+print('')
+print('UI MASTER BUT NOT LIVE THEN')
+print('[i for i in b if i not in a]')
+x = ([i for i in b if i not in a])
+print(len(x), x)
+print('')
+print('==========================================')
+print('NOW VS UI MASTER')
+print('==========================================')
+print('')
+print('LIVE NOW AND IN UI MASTER')
+print('[i for i in c if i in b]')
+x = ([i for i in c if i in b])
+print(len(x), x)
+print('')
+print('LIVE NOW NOT IN UI MASTER')
+print('[i for i in c if i not in b]')
+x = ([i for i in c if i not in b])
+print(len(x), x)
+print('')
+print('UI MASTER BUT NOT LIVE NOW')
+print('[i for i in b if i not in c]')
+x = ([i for i in b if i not in c])
+print(len(x), x)
+print('')
+print('==========================================')
+print('==========================================')
+print('')
+print('ALL STARS - THEN AND NOW AND UI MASTER')
+print('[i for i in a if ((i in b) and (i in c))]')
+x = ([i for i in a if ((i in b) and (i in c))])
+print(len(x), x)
+print('')
